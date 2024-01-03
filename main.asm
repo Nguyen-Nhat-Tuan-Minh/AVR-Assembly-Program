0be267554440cc66b9d9ee89b1ffc73ccbecf014
Minh Nguyen <145042210+Nguyen-Nhat-Tuan-Minh@users.noreply.github.com>
2024-01-03 10:12:02 +0700

    Add files via upload

---
+++ main.asm
@@ -0,0 +1,19 @@
+;
+; PEX5.asm
+;
+; Created: 9/16/2023 8:07:56 PM
+; Author : tuan minh
+;
+
+
+; Replace with your application code
+		CBI DDRA, 0
+		SBI PORTA, 0
+		SBI DDRA, 1
+MAIN:	SBIC PINA, 0
+		RJMP ON
+		SBI PORTA, 1
+		RJMP MAIN
+ON:		CBI PORTA, 1
+		RJMP MAIN
+
