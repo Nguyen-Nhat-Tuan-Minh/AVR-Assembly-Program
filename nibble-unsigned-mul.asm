902de29b3f38b12c0c842de3f9c694f4e862d453
Minh Nguyen <145042210+Nguyen-Nhat-Tuan-Minh@users.noreply.github.com>
2024-01-03 10:09:04 +0700

    Read one byte, multiply two nibbles unsigned, output result

--- nibble-unsigned-mul.asm
+++ nibble-unsigned-mul.asm
@@ -14,12 +14,13 @@
 			OUT PORTA, R16	; Pull-up config.
 			
 			LDI R17, 0x00	; Initialize low nibble holder
-REWIND:		LDI R18, 0x04 	; Initialize shift counter
+REWIND:			LDI R18, 0x04 	; Initialize shift counter
 			IN R16, PINA
 			COM R16			; Pull-up config.
 			MOV R17, R16	; R17 holds the same content as R16
 			ANDI R17, 0x0F	; R17 now holds low nibble of R16
-HIGH_NIB:	LSR R16 
+
+HIGH_NIB:		LSR R16 
 			DEC R18
 			BRNE HIGH_NIB	; Done high nibble extraction
 
